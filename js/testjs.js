bf456867ab2015058c3a13bdbcc84b20e265ec52
Rahul Jain <rahuljait@gmail.com>
2021-07-25 01:58:51 +0530

    simple bulb game in html css and javascript

---
+++ js/testjs.js
@@ -0,0 +1,8 @@
+function switchon(){
+    var oldid= document.getElementById("clip");
+    oldid.id = "clip2";
+}
+function switchoff(){
+    var oldid= document.getElementById("clip2");
+    oldid.id = "clip";
+}
